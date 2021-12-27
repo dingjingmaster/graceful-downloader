861235fcaef053629c8a628d410de414f189c944
dingjing <dingjing@live.cn>
2021-12-27 17:22:52 +0800

    response

--- core/http-respose.c
+++ core/http-respose.c
@@ -1,2 +1,23 @@
 #include "http-respose.h"
 
+
+HttpResopnse *http_respose_new()
+{
+    HttpResopnse* resp = g_malloc0 (sizeof (HttpResopnse));
+    if (!resp) {
+        return NULL;
+    }
+
+    return resp;
+}
+
+void http_respose_destroy(HttpResopnse *resp)
+{
+    g_return_if_fail (resp);
+
+    if (resp->reason)       g_free (resp->reason);
+    if (resp->headers)      http_header_list_destroy (resp->headers);
+    if (resp->body)         g_free (resp->body);
+
+    g_free (resp);
+}
--- core/http-respose.h
+++ core/http-respose.h
@@ -7,4 +7,22 @@
 #include "http-header.h"
 
 
+typedef struct _HttpRespose         HttpResopnse;
+
+
+struct _HttpRespose
+{
+    float               httpVersion;
+    int                 statusCode;
+    char               *reason;
+    HttpHeaderList     *headers;
+    char               *body;
+    int                 bodyLen;
+};
+
+
+HttpResopnse*   http_respose_new ();
+void            http_respose_destroy (HttpResopnse* resp);
+
+
 #endif // HTTPRESPOSE_H
